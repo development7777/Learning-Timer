bc57d0da1cd01e1fdc53938cddb01ec7fc7d6b61
development7777 <kodai819@outlook.com>
2025-07-18 13:30:11 +0900

    タイマーアプリを実装

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './timer.png'
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
